a772398c397f540fe975228b4b8088ec1caa265c
zoducminh <zoducminh@gmail.com>
2023-06-11 20:23:45 +0700

    final

--- site/js/login.js
+++ site/js/login.js
@@ -1,40 +1,43 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-    import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
-    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyAUoAcUSnOxUJK_RvQZpXIEUimyGOULg9o",
-        authDomain: "nps-jsi09-9b9fe.firebaseapp.com",
-        databaseURL: "https://nps-jsi09-9b9fe-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "nps-jsi09-9b9fe",
-        storageBucket: "nps-jsi09-9b9fe.appspot.com",
-        messagingSenderId: "341512619162",
-        appId: "1:341512619162:web:ed8645d7eff740116b4b23",
-        measurementId: "G-3K8YDGFWPD"
-      };
+const firebaseConfig = {
+  apiKey: "AIzaSyAUoAcUSnOxUJK_RvQZpXIEUimyGOULg9o",
+  authDomain: "nps-jsi09-9b9fe.firebaseapp.com",
+  databaseURL: "https://nps-jsi09-9b9fe-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nps-jsi09-9b9fe",
+  storageBucket: "nps-jsi09-9b9fe.appspot.com",
+  messagingSenderId: "341512619162",
+  appId: "1:341512619162:web:ed8645d7eff740116b4b23",
+  measurementId: "G-3K8YDGFWPD"
+};
 
-      const app = initializeApp(firebaseConfig);
-      const database = getDatabase(app);
-      const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
 
 let login = document.getElementById('login')
 login.addEventListener('click', (e) => {
 
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    signInWithEmailAndPassword(auth, email, password)
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
-      
+
       const dt = new Date();
-      update(ref(database, 'users/' + user.uid),{
-        last_login : dt,
+      update(ref(database, 'users/' + user.uid), {
+        last_login: dt,
+        email: email
       })
-      alert('User loged in!');
-      //
-      window.location = "http://127.0.0.1:5500/site/index.html"
+        .then(() => {
+          alert('User loged in!');
+          //
+          sessionStorage.setItem('userId',user.uid)
+          window.location = "http://127.0.0.1:5500/site/index.html"
+        });
       // ...
     })
     .catch((error) => {
@@ -44,5 +47,5 @@ login.addEventListener('click', (e) => {
       alert(errorMessage);
 
     });
-   
+
 });
--- site/js/register.js
+++ site/js/register.js
@@ -1,38 +1,47 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-    import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
-    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyAUoAcUSnOxUJK_RvQZpXIEUimyGOULg9o",
-        authDomain: "nps-jsi09-9b9fe.firebaseapp.com",
-        databaseURL: "https://nps-jsi09-9b9fe-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "nps-jsi09-9b9fe",
-        storageBucket: "nps-jsi09-9b9fe.appspot.com",
-        messagingSenderId: "341512619162",
-        appId: "1:341512619162:web:ed8645d7eff740116b4b23",
-        measurementId: "G-3K8YDGFWPD"
-      };
+const firebaseConfig = {
+  apiKey: "AIzaSyAUoAcUSnOxUJK_RvQZpXIEUimyGOULg9o",
+  authDomain: "nps-jsi09-9b9fe.firebaseapp.com",
+  databaseURL: "https://nps-jsi09-9b9fe-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nps-jsi09-9b9fe",
+  storageBucket: "nps-jsi09-9b9fe.appspot.com",
+  messagingSenderId: "341512619162",
+  appId: "1:341512619162:web:ed8645d7eff740116b4b23",
+  measurementId: "G-3K8YDGFWPD"
+};
 
-    const app = initializeApp(firebaseConfig);
-    const database = getDatabase(app);
-    const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
 let signUp = document.getElementById('signUp');
-        signUp.addEventListener('click',(e) => {
+signUp.addEventListener('click', (e) => {
+  let accountType = document.querySelector('.selectAccount').value
+  let gender = document.querySelector('.selectGender').value
 
-          var email = document.getElementById('email').value;
-          var password = document.getElementById('password').value;
+  debugger
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
 
 
-          createUserWithEmailAndPassword(auth, email, password)
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
 
-      set(ref(database, 'users/' + user.uid),{
-        email:email,
+      set(ref(database, 'users/' + user.uid), {
+        accountType: accountType,
+        gender: gender,
+        email: email,
+        password: password
       })
-      alert('User created!');
-      window.location = "http://127.0.0.1:5500/site/index.html"
+        .then(() => { //em thiếu cái then này
+          alert('User created!');
+          window.location = "http://127.0.0.1:5500/site/index.html"
+        })
+
       // ...
     })
     .catch((error) => {
@@ -42,4 +51,4 @@ let signUp = document.getElementById('signUp');
       alert(errorMessage);
       // ..
     });
-    });
+});
